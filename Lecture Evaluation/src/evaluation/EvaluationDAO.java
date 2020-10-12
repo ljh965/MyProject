218d7c21c2c924023ed8c39c70709b58954b472e
ljh965 <aslk94@naver.com>
2020-10-12 16:30:04 +0900

    practice

--- Lecture Evaluation/src/evaluation/EvaluationDAO.java
+++ Lecture Evaluation/src/evaluation/EvaluationDAO.java
@@ -3,7 +3,9 @@ package evaluation;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import user.UserDTO;
 import util.DatabaseUtil;
 
 public class EvaluationDAO {
@@ -36,5 +38,82 @@ public class EvaluationDAO {
 			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();} 
 		}
 		return -1; // 데이터베이스 오류
+	}
+	
+	public ArrayList<EvaluationDTO> getList(String lectureDivide, String searchType, String search, int pageNumber){
+		if(lectureDivide.equals("전체")) {
+			lectureDivide = "";
+		}
+		ArrayList<EvaluationDTO> evaluationList = null;
+		String SQL = "";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			if(searchType.equals("최신순")) {
+				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE"+
+						"? ORDER BY evaluationID DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+			}
+			else if(searchType.equals("추천순")) {
+				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE"+
+						"? ORDER BY likeCount DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+			}
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, "%" + lectureDivide + "%");
+			pstmt.setString(2, "%" + search + "%");
+			rs = pstmt.executeQuery();
+			evaluationList = new ArrayList<EvaluationDTO>();
+
+			while(rs.next()) {
+				EvaluationDTO evalutation = new EvaluationDTO(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						rs.getInt(5),
+						rs.getString(6),
+						rs.getString(7),
+						rs.getString(8),
+						rs.getString(9),
+						rs.getString(10),
+						rs.getString(11),
+						rs.getString(12),
+						rs.getString(13),
+						rs.getInt(14));
+				evaluationList.add(evalutation);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null) conn.close();} catch (Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();} 
+			try {if(rs != null) rs.close();} catch (Exception e) {e.printStackTrace();} 
+		}
+		return evaluationList;
+	}
+	
+	public int join(UserDTO user) { // 회원가입
+		String SQL = "INSERT INTO USER VALUES(?,?,?,?, false)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserEmail());
+			pstmt.setString(4, user.getUserEmailHash());
+			
+			return pstmt.executeUpdate();	
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null) conn.close();} catch (Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();} 
+		}
+		return -1; // 회원가입 실패
 	}
 }
