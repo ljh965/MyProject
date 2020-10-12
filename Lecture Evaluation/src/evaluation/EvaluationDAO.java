855b41ccbdae7db67135f8d15d3f268e2329c09e
ljh965 <aslk94@naver.com>
2020-10-12 18:19:06 +0900

    practice

--- Lecture Evaluation/src/evaluation/EvaluationDAO.java
+++ Lecture Evaluation/src/evaluation/EvaluationDAO.java
@@ -94,26 +94,60 @@ public class EvaluationDAO {
 		return evaluationList;
 	}
 	
-	public int join(UserDTO user) { // 회원가입
-		String SQL = "INSERT INTO USER VALUES(?,?,?,?, false)";
+	public int like(String evaluationID) { // 추천
+		String SQL = "UPDATE EVALUATION SET likeCount = likeCount + 1 WHERE evaluationID = ?";
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		
 		try {
 			conn = DatabaseUtil.getConnection();
 			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1, user.getUserID());
-			pstmt.setString(2, user.getUserPassword());
-			pstmt.setString(3, user.getUserEmail());
-			pstmt.setString(4, user.getUserEmailHash());
-			
-			return pstmt.executeUpdate();	
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			return pstmt.executeUpdate();
 		}catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {if(conn != null) conn.close();} catch (Exception e) {e.printStackTrace();}
-			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();} 
+			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();}
 		}
-		return -1; // 회원가입 실패
+		return -1;
+	}
+	public int delete(String evaluationID) { // 삭제
+		String SQL = "DELETE FROM EVALUATION WHERE evaluationID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null) conn.close();} catch (Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();}
+		}
+		return -1; // 데이터베이스 오류
+	}
+	public String getUserID(String evaluationID) { // 삭제
+		String SQL = "SELECT userID FROM EVALUATION WHERE evaluationID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null) conn.close();} catch (Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch (Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch (Exception e) {e.printStackTrace();} 
+		}
+		return null; // 존재하지 않는 아이디
 	}
 }
